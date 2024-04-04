0722e2407021c893eceb9ab3739264171311b06f
AgustianAr <112686936+AgustianAr@users.noreply.github.com>
2024-04-04 22:04:59 +0800

    Test

---
+++ navbar1.php
@@ -0,0 +1,25 @@
+
+<style>
+	.collapse a{
+		text-indent:10px;
+	}
+	nav#sidebar{
+		background: url(assets/uploads/<?php echo $_SESSION['system']['cover_img'] ?>) !important
+	}
+</style>
+
+<nav id="sidebar" class='mx-lt-5 bg-dark' >
+		
+		<div class="sidebar-list">
+				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+				<a href="index.php?page=courses" class="nav-item nav-courses"><span class='icon-field'><i class="fa fa-list"></i></span> Request List</a>
+		</div>
+
+</nav>
+<script>
+	$('.nav_collapse').click(function(){
+		console.log($(this).attr('href'))
+		$($(this).attr('href')).collapse()
+	})
+	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+</script>
